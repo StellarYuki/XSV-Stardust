608934974b5008c9608b3d3c85032ec7d0f58dae
StellarYuki <tailbluex@gmail.com>
2026-07-17 03:49:59 +0900

    Create db_ship.js

---
+++ db_ship.js
@@ -0,0 +1,11 @@
+const shipStatsDB = [
+    { label: "Hull Integrity", value: "100%" },
+    { label: "Shield Emitters", value: "100%" },
+    { label: "Warp Fuel (Plasma)", value: "85%" },
+    { label: "Impulse Fuel", value: "92%" },
+    { label: "Life Support (O2)", value: "98%" },
+    { label: "Water Reclamation", value: "RESTRICTED" },
+    { label: "Micro-Torpedoes", value: "12 / 12" },
+    { label: "Cargo Bay Capacity", value: "14 / 50 Tons" },
+    { label: "Ship's Credits (ISRB)", value: "45,000 CR" }
+];
