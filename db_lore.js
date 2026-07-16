44fc7849c73937bb746e79d1459e1457e99d90ea
StellarYuki <tailbluex@gmail.com>
2026-07-17 04:21:35 +0900

    Create db_lore.js

---
+++ db_lore.js
@@ -0,0 +1,14 @@
+const loreStorageDB = [
+    {
+        id: "report_01",
+        title: "Mission Directive: Operation Abruix",
+        date: "5092 S.Y.",
+        content: "We have secured a shadow-contract to investigate a massive, ancient energy signature emanating from the uncharted Var Lupra system. The signal is coming from Pareah—a hostile, toxic jungle world. Prepare the ship for hazardous navigation through the Varix Gas Clouds."
+    },
+    {
+        id: "lore_astral",
+        title: "Astral Supply Co. Overview",
+        date: "Databank Entry",
+        content: "An interstellar medium-sized freight company specializing in hazardous materials. Family-owned by the Tora-katasu clan. They operate a private fleet of transport and security ships, maintaining the Night Star and Day Star stations."
+    }
+];
