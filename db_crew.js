d85ff1ed38f9a82aca15cc243b8a5af5905447f5
StellarYuki <tailbluex@gmail.com>
2026-07-17 05:08:36 +0900

    Update db_crew.js

--- db_crew.js
+++ db_crew.js
@@ -5,7 +5,7 @@ const crewDB = [
         dept: "Command & CONN (Blue)",
         bio: "The commanding officer of the XSV Stardust. Runs a tight ship and ensures the ISRB contracts get paid.",
         sl_link: "secondlife:///app/agent/5c8a32d3-0b7b-464d-bd1e-d23253e60d1e/about",
-        sl_pic: "https://via.placeholder.com/100/000000/ff9900?text=PIC" // Replace with your image link!
+        sl_pic: "images/stellar.jpg" // Make sure the filename matches what you uploaded!
     },
     {
         name: "Commander Roman von Ungern-Sternberg",
@@ -13,7 +13,7 @@ const crewDB = [
         dept: "Command / First Officer",
         bio: "Second in command. Handles ship operations and ensures the crew follows protocol.",
         sl_link: "secondlife:///app/agent/0847548f-36a5-4dc7-8dee-d296dbbabe32/about",
-        sl_pic: "https://via.placeholder.com/100/000000/cc66ff?text=PIC"
+        sl_pic: "images/roman.jpg"
     },
     {
         name: "Lieutenant Aika Yasuda",
@@ -21,6 +21,6 @@ const crewDB = [
         dept: "Operations",
         bio: "Key crewmember handling vital ship systems and away missions.",
         sl_link: "secondlife:///app/agent/857930c8-f80e-4107-b5a6-edb14d5c5eec/about",
-        sl_pic: "https://via.placeholder.com/100/000000/99ccff?text=PIC"
+        sl_pic: "images/aika.jpg"
     }
 ];
